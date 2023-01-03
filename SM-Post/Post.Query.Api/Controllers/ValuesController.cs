e7621b6f8614f54c439acda73e6d5abc58ed612a
Rosteslav Yordanov <rosteslav.yordanov@gmail.com>
2023-01-03 21:23:48 +0200

    Basic Structure

---
+++ SM-Post/Post.Query.Api/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Post.Query.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
